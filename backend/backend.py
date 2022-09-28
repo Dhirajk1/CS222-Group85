f3e815224ef66b353794d58d461050281b3900f9
kulksoh21 <pingsoham@gmail.com>
2022-09-28 15:44:41 -0500

    sign up basic is templated out

---
+++ backend/backend.py
@@ -0,0 +1,15 @@
+def signup():
+    if(request.method == 'GET'):
+        return render_template('signup.html')
+
+    else:
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password = request.form.get('password')
+        user = User.query.filter_by(email = email).first()
+
+        if user:
+            flash('Email has already been used')
+            return redirect(url_for('auth.signup'))
+
+        new_user = User(email = email, name=name, password=password)
