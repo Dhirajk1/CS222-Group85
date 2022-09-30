b68b4edc3577376c7876c2e81cf6937b966c4714
AryanMashruwala <amash2@illinois.edu>
2022-09-30 16:15:03 -0500

    app_init_incomplete

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,22 @@
-import flask
-from backend import ???
-@app.route("")
-def funtion(path):
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_login import UserMixin
+
+database = SQLAlchemy()
+
+app = Flask(__name__) 
+app.config["SECRET_KEY"] = "cs222"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.sqlite"
+
+database.init.app(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(database.Model, UserMixin):
+    identification = database.Column(database.Integer, primary_key = True)
+    username = database.Column(database.String())
+
+if __name__ == "__main__":
+    app.run(debug = True)
