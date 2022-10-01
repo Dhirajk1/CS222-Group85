1586adccd8a1dfec93a198d4446658adfa662924
kulksoh21 <pingsoham@gmail.com>
2022-09-30 21:22:02 -0500

    Added final lint check fix

--- backend/test.py
+++ backend/test.py
@@ -2,6 +2,8 @@ from app import *
 import requests
 
 def signup_test():
+
+    """Test case for signup"""
     req = requests.post("http://127.0.0.1:5000", {"email" : "sohamsk2@illinois.edu", "username" : "kulksoh21", "password" : "sohamcs222"})
     print(req.content)
 
