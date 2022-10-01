ab9f16b5efe258c7b03dd2f428edcabb6802c86a
AryanMashruwala <amash2@illinois.edu>
2022-09-30 19:23:46 -0500

    Errors resolved

--- backend/app.py
+++ backend/app.py
@@ -1,38 +1,53 @@
-import json
+"""imports of necessary modules for app initilization and user class functionality"""
 from flask import Flask
 from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_login import UserMixin
 
-from admin.login_ import login_
-
 database = SQLAlchemy()
 
 app = Flask(__name__)
-app.register_blueprint(login_, url_prefix='')
 
 app.config["SECRET_KEY"] = "cs222"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.sqlite"
 
-database.init.app(app)
+from app import database
 
-login_manager = LoginManager()
-login_manager.init_app(app)
 
 class UserClass(database.Model, UserMixin):
-    identification = database.Column(database.Integer, primary_key = True)
+    """
+    user class that inherits from UserMixin for default methods
+    for flask login lib and from the SQLAlchemy
+    """
+    identification = database.Column(database.String(99), primary_key = True)
     username = database.Column(database.String(12), unique = True)
     email = database.Column(database.String(99), unique = True)
     password = database.Column(database.String(99))
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
 @login_manager.user_loader
 def load_user(user_id):
+    """
+    required method for flask login lib functionality that returns
+    the userid
+    """
     return UserClass.get(user_id)
 
 @app.route('/')
 def calendar_home():
+    """
+    meant to open up a standard blankslate homepage for now
+    """
     return jsonify({"Loaded calendar page" : True})
+
+
+from login import login_
+
+app.register_blueprint(login_, url_prefix='')
 
 if __name__ == "__main__":
     app.run(debug = True)
--- backend/login.py
+++ backend/login.py
@@ -1,29 +1,31 @@
-import json
-from flask import Flask
+"""imports of necessary modules for login/logout functionality"""
 from flask import jsonify
 from flask import Blueprint
 from flask import request
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import check_password_hash
+from app import UserClass
 
 login_ = Blueprint("login_", __name__)
 
-@app.route("/login", methods = ["GET", "POST"])
-def login(): 
+#login route, handles get/post methods
+@login_.route("/login", methods = ["GET", "POST"])
+def login():
+
+    """login function that checks credentials"""
+
     if request.method == "POST":
         password = request.form.get("password")
         email = request.form.get("email")
         user = UserClass.query.filter_by(email = email).first()
-    elif request.method = "GET":
+    elif request.method == "GET":
         return jsonify({"Return login page" : True})
-
-    
     if not check_password_hash(user.password, password) and not user:
         return jsonify({"Login Fail, username and password are incorrect or don't match" : False})
-
     return jsonify({"Login True" : True})
 
-@app.route("/logout")
+@login_.route("/logout")
 def logout():
+
+    """function that logs the user out, outputs if successful"""
+
     return jsonify({{"Logout Success": True}})
-
-
