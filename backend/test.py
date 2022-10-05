65bb88fc7a4f5dedea91de0caf81322610ccd500
kulksoh21 <pingsoham@gmail.com>
2022-10-04 19:48:56 -0500

    Merged login and sign up for backend

--- backend/test.py
+++ backend/test.py
@@ -6,6 +6,7 @@ def signup_test():
     """Test case for signup"""
     req = requests.post("http://127.0.0.1:5000", {"email" : "sohamsk2@illinois.edu", "username" : "kulksoh21", "password" : "sohamcs222"})
     print(req.content)
+    
 
 
 def login_test():
