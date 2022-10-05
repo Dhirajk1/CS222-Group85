3deec73088dba4d75b73010fdc19797cb4349db2
kulksoh21 <pingsoham@gmail.com>
2022-10-04 19:37:19 -0500

    Merging of login and signup backend work

--- backend/test.py
+++ backend/test.py
@@ -8,6 +8,16 @@ def signup_test():
     print(req.content)
 
 
+def login_test():
+    """tests basic login functionality"""
+    req = requests.post("http://127.0.0.1:5000", {"email" : "amash2@illinois.edu", "username" : "Aryan", "password" : "cs222Aryan"})
+    print(req.content)
+
+
+def calendar_home_test():
+    """tests homepage"""
+    assert calendar_home() == jsonify({"Loaded Calendar page" : True})
+
 
 
 
