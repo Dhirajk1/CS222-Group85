90da2b65b3e21506f97f4ae71afe98257ed74116
Dhiraj Kuttichirayil <dhirajk1@gmail.com>
2022-10-13 20:45:39 -0500

    finish calendar and related utilities :)

--- backend/user_calendar.py
+++ backend/user_calendar.py
@@ -2,27 +2,9 @@
 Python objects for storing the calendar
 """
 import datetime
-from typing import List
+from typing import List, Tuple
 from dataclasses import dataclass
-from .calendar_utils import file_is_good, parse_ical
-
-
-@dataclass
-class CalendarEntry:
-    """
-    Object to store entries
-    """
-
-    title: str
-    start: datetime
-    end: datetime
-
-    def to_str(self) -> str:
-        """
-        String representation of the Calendar Entry
-        """
-        return f"Event: {self.title}; \
-            Starts at: {self.start.strftime('%c')}; Ends at {self.end.strftime('%c')}"
+from calendar_utils import CalendarEntry, file_is_good, parse_ical
 
 
 class UserCalendar:
@@ -54,16 +36,15 @@ class UserCalendar:
         Recieves an ical file, parses it and adds it to the calendar
         """
         if file_is_good(i_cal_file):
-            self.time_details, self.busy = parse_ical(i_cal_file)
+            new_events = parse_ical(i_cal_file)
+            self.entries.extend(new_events)
 
     def print(self):
         """
         Print the user and times
         """
-        print(f"User: {self.user_id}")
-        print("\nTimes:")
-        print([time.strftime("%c") for time in self.busy])
-        print(self.time_details)
+        for entry in self.entries:
+            print(entry.to_str())
 
     def get_user(self) -> str:
         """
@@ -71,14 +52,48 @@ class UserCalendar:
         """
         return self.user_id
 
-    def get_busy_times(self) -> List[datetime.datetime]:
+    def get_entries(self) -> List[CalendarEntry]:
         """
-        Returns a list of all busy times
+        Returns a list of all entries
         """
-        return self.busy
+        return self.entries
 
-    def get_event_details(self) -> List[str]:
+    def get_db_input(self) -> Tuple[str, str]:
         """
-        Return a list of all the event's details
+        Return strings for database input
         """
-        return self.time_details
+        titles_lst = []
+        times_lst = []
+        for event in self.entries:
+            titles_lst.append(event.title)
+            times_lst.append(f"{event.start.isoformat()}=>{event.end.isoformat()}")
+
+        return ",".join(titles_lst), ",".join(times_lst)
+
+
+if __name__ == "__main__":
+
+    @dataclass
+    class TestInput:
+        """
+        Class to feed in a test input (w/out database entry)
+        """
+
+        user_id: str
+        details: str
+        times: str
+
+    short_input = TestInput(
+        times="2022-08-25T09:00:00-05:00=>2022-08-25T11:30:00-05:00",
+        user_id="myUser:)",
+        details="EVENT",
+    )
+    sample_calendar = UserCalendar(short_input)
+    sample_calendar.print()
+
+    sample_calendar.import_ical("./ICS Parsing Sample/uiuc_calendar.ics")
+    db_details, db_times = sample_calendar.get_db_input()
+
+    output_to_recreate = TestInput("1234", db_details, db_times)
+    recreated_calendar = UserCalendar(output_to_recreate)
+    recreated_calendar.print()
