6ebfc7fee24aa16f06a638afc39fa1666b16d296
kulksoh21 <pingsoham@gmail.com>
2022-11-17 21:01:29 -0600

    Created multiple checkbox funcionality

--- src/pages/check_box/create_check_box.test.tsx
+++ src/pages/check_box/create_check_box.test.tsx
@@ -5,23 +5,26 @@ test('Initial conditions', () => {
     render(<MyCheckbox />)
 
     // Check that the checkbox starts out unchecked
-    const checkbox = screen.getByRole('checkbox')
-    expect(checkbox).not.toBeChecked()
+    const checkboxes = screen.getAllByRole('checkbox')
+    expect(checkboxes[0]).not.toBeChecked()
+    expect(checkboxes[1]).not.toBeChecked()
   })
 
   test('After checking', () => {
     render(<MyCheckbox />)  
     // Check that the checkbox shows checked
-    const checkbox = screen.getByRole('checkbox')
-    fireEvent.click(checkbox)
-    expect(checkbox).toBeChecked()
+    const checkboxes = screen.getAllByRole('checkbox')
+    fireEvent.click(checkboxes[0])
+    expect(checkboxes[0]).toBeChecked()
+    expect(checkboxes[1]).not.toBeChecked()
   })
     
-  it('displays a happy face', () => {
+  it('displays a happy face only if both are checked', () => {
     //only displays happy face if checkbox is clicked
       render(<MyCheckbox />)
-      const checkbox = screen.getByRole('checkbox')
-      fireEvent.click(checkbox)
+      const checkboxes = screen.getAllByRole('checkbox')
+      fireEvent.click(checkboxes[0])
+      fireEvent.click(checkboxes[1])
       const displayedImage = screen.getByRole('img');
       expect(displayedImage).toBeVisible();
   })
--- src/pages/check_box/create_check_box.tsx
+++ src/pages/check_box/create_check_box.tsx
@@ -4,10 +4,15 @@ import { text } from 'stream/consumers';
 
 //this is the checkbox type that I created
 const MyCheckbox = () => {
-  const [checked, setChecked] = React.useState(false);
+  const [checked1, setChecked1] = React.useState(false);
+  const [checked2, setChecked2] = React.useState(false);
   //toggles the state of checked
-  const handleChange = () => {
-    setChecked(!checked);
+  const handleChangeOne = () => {
+    setChecked1(!checked1);
+  };
+  //handles second checkbox
+  const handleChangeTwo = () => {
+    setChecked2(!checked2);
   };
   //handles changes and also displays image if checkbox is clicked
   return (
@@ -15,14 +20,25 @@ const MyCheckbox = () => {
       <label>
         <Checkbox
           label="My Value"
-          value={checked}
-          onChange={handleChange}
+          value={checked1}
+          onChange={handleChangeOne}
         />
         My Value
       </label>
-      <p>Is &lsquo;My Value&lsquo; checked? {checked.toString()}</p>
+      <label>
+        <Checkbox
+          label="Value 2"
+          value={checked2}
+          onChange={handleChangeTwo}
+        />
+        Value 2
+      </label>
+      <p>Is &lsquo;My Value&lsquo; checked? {checked1.toString()}</p>
       {
-        checked? <img alt="happy face" src={'./happy.jpeg'}/> :null
+        checked1?
+        (
+            checked2? <img alt="happy face" src={'./happy.jpeg'}/> :null
+        ):null
       }
     </div>
   );
